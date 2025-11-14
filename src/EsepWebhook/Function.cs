5d381ecc587c7612a9419844c84b19c96882a047
p-iteghie <piteghie@outlook.com>
2025-11-13 20:17:54 -0500

    Fixed Lambda function.

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -20,6 +20,15 @@ public class Function
 
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
-        return input.ToUpper();
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+    
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
     }
 }
