26a604511538aa4c5cec12f9d9d3133a778a40f4
p-iteghie <piteghie@outlook.com>
2025-11-13 22:15:38 -0500

    Fixed

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,7 +1,6 @@
-using Amazon.Lambda.Core;
 using System.Text;
+using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,34 +9,30 @@ namespace EsepWebhook;
 
 public class Function
 {
-    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(object input, ILambdaContext context)
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input.Body}");
+        context.Logger.LogInformation($"FunctionHandler received: {input}");
 
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.Body);
-
-        var message = new
-        {
-            text = $"Issue Created: {json.issue.html_url}"
-        };
-        string payload = JsonConvert.SerializeObject(message);
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
         var client = new HttpClient();
-        
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
-
+    
         var response = client.Send(webRequest);
-
-        return new APIGatewayProxyResponse
-        {
-            StatusCode = 200,
-            Body = "Message sent to Slack",
-            Headers = new Dictionary<string, string> { 
-                { "Content-Type", "application/json" } 
-            }
-        };
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
     }
 }
