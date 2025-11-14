190865ae16c337fb83850154688d9e1317b7e38b
p-iteghie <piteghie@outlook.com>
2025-11-13 21:22:24 -0500

    Fixed again

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,18 +1,24 @@
-using Amazon.Lambda.Core;
 using System.Text;
-using Newtonsoft.Json;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EsepWebhook;
 
 public class Function
 {
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
     public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input.Body}");
-
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.Body);
 
         var message = new
